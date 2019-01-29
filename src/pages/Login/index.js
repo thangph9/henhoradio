299424f5ed3bbeb8064cc6a3519587689fc97314
trjvjp97 <trjvjp97@gmail.com>
2019-01-29 12:31:08 +0700

    123

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,3 +1,6 @@
+/* eslint-disable arrow-body-style */
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable class-methods-use-this */
 /* eslint-disable react/no-multi-comp */
 /* eslint-disable react/no-access-state-in-setstate */
 /* eslint-disable react/no-unused-state */
@@ -9,10 +12,11 @@
 /* eslint-disable prefer-template */
 import React, { PureComponent } from 'react';
 import { connect } from 'dva';
-import { Form, Input, Icon, Button, Popover, Progress } from 'antd';
+import { Form, Input, Icon, Button, Popover, Progress, Select } from 'antd';
 // import {Link} from 'react-router-dom'
-
 import styles from './styles.less';
+
+const { Option } = Select;
 
 @connect(({ loading, authentication }) => ({
   submitting: loading.effects['form/submitRegularForm'],
@@ -61,19 +65,21 @@ class FormLogin extends PureComponent {
     const { getFieldDecorator } = this.props.form;
     return (
       <Form onSubmit={this.handleSubmit}>
-        <Form.Item>
-          {getFieldDecorator('username', {
+        <Form.Item label="Số điện thoại" style={{ marginBottom: '0px' }}>
+          {getFieldDecorator('phone', {
             rules: [
               {
                 required: true,
-                message: 'Vui lòng nhập Email!',
+                message: 'Yêu cầu nhập số điện thoại',
+              },
+              {
+                pattern: /\d{9}$/,
+                message: 'Nhập sai định dạng hoặc chưa đủ chữ số！',
               },
             ],
-          })(
-            <Input size="large" placeholder="Tên đăng nhập" onBlur={e => this.handleBlurEmail(e)} />
-          )}
+          })(<Input size="large" />)}
         </Form.Item>
-        <Form.Item>
+        <Form.Item label="Mật khẩu">
           {getFieldDecorator('password', {
             rules: [
               {
@@ -81,14 +87,25 @@ class FormLogin extends PureComponent {
                 message: 'Vui lòng nhập mật khẩu!',
               },
             ],
-          })(<Input type="password" size="large" placeholder="Mật khẩu" />)}
+          })(<Input type="password" size="large" />)}
         </Form.Item>
-
         <Form.Item>
-          <Button type="primary" htmlType="submit" block>
+          <Button
+            size="large"
+            type="primary"
+            style={{
+              background: '#45c000',
+              borderColor: '#45c000',
+              color: '#fff',
+              borderRadius: '30px',
+            }}
+            htmlType="submit"
+            block
+          >
             Đăng nhập
           </Button>
         </Form.Item>
+        <a href="/forgot">Quên mật khẩu?</a>
       </Form>
     );
   }
@@ -111,6 +128,227 @@ class FormRegister extends PureComponent {
   state = {
     visible: false,
     help: '',
+    month_selected: [],
+    year: [
+      2019,
+      2018,
+      2017,
+      2016,
+      2015,
+      2014,
+      2013,
+      2012,
+      2011,
+      2010,
+      2009,
+      2008,
+      2007,
+      2006,
+      2005,
+      2004,
+      2003,
+      2002,
+      2001,
+      2000,
+      1999,
+      1998,
+      1997,
+      1996,
+      1995,
+      1994,
+      1993,
+      1992,
+      1991,
+      1990,
+      1989,
+      1988,
+      1987,
+      1986,
+      1985,
+      1984,
+      1983,
+      1982,
+      1981,
+      1980,
+      1979,
+      1978,
+      1977,
+      1976,
+      1975,
+      1974,
+      1973,
+      1972,
+      1971,
+      1970,
+      1969,
+      1968,
+      1967,
+      1966,
+      1965,
+      1964,
+      1963,
+      1962,
+      1961,
+      1960,
+      1959,
+      1958,
+      1957,
+      1956,
+      1955,
+      1954,
+      1953,
+      1952,
+      1951,
+      1950,
+      1949,
+      1948,
+      1947,
+      1946,
+      1945,
+      1944,
+      1943,
+      1942,
+      1941,
+      1940,
+      1939,
+      1938,
+      1937,
+      1936,
+      1935,
+      1934,
+      1933,
+      1932,
+      1931,
+      1930,
+      1929,
+    ],
+    month: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    day_in_month_full: [
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      10,
+      11,
+      12,
+      13,
+      14,
+      15,
+      16,
+      17,
+      18,
+      19,
+      20,
+      21,
+      22,
+      23,
+      24,
+      25,
+      26,
+      27,
+      28,
+      29,
+      30,
+      31,
+    ],
+    day_in_month_short: [
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      10,
+      11,
+      12,
+      13,
+      14,
+      15,
+      16,
+      17,
+      18,
+      19,
+      20,
+      21,
+      22,
+      23,
+      24,
+      25,
+      26,
+      27,
+      28,
+      29,
+      30,
+    ],
+    day_in_february_normal: [
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      10,
+      11,
+      12,
+      13,
+      14,
+      15,
+      16,
+      17,
+      18,
+      19,
+      20,
+      21,
+      22,
+      23,
+      24,
+      25,
+      26,
+      27,
+      28,
+    ],
+    day_in_february_profit: [
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      10,
+      11,
+      12,
+      13,
+      14,
+      15,
+      16,
+      17,
+      18,
+      19,
+      20,
+      21,
+      22,
+      23,
+      24,
+      25,
+      26,
+      27,
+      28,
+      29,
+    ],
   };
 
   getPasswordStatus = () => {
@@ -128,15 +366,33 @@ class FormRegister extends PureComponent {
   handleSubmit = e => {
     e.preventDefault();
     const { form } = this.props;
-    const email = this.props.form.getFieldValue('email');
-    const filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-    if (!filter.test(email)) {
-      return;
+    // const email = this.props.form.getFieldValue('email');
+    // const filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    // if (!filter.test(email)) {
+    // return;
+    // }
+    const day = this.props.form.getFieldValue('day');
+    const month = this.props.form.getFieldValue('month');
+    const year = this.props.form.getFieldValue('year');
+    const gender = this.props.form.getFieldValue('gender');
+    if (!gender) {
+      this.props.form.setFields({
+        gender: {
+          errors: [new Error('Chọn giới tính!')],
+        },
+      });
+    }
+    if (!day || !month || !year) {
+      this.props.form.setFields({
+        day: {
+          errors: [new Error('Chọn đầy đủ ngày sinh!')],
+        },
+      });
     }
     form.validateFields((err, values) => {
       // if (this.state.value.length > 0) {
       if (values.password === values.repassword) {
-        if (!err) {
+        if (!err && day && month && year && gender) {
           console.log(values);
         }
       }
@@ -229,46 +485,254 @@ class FormRegister extends PureComponent {
     }
   }
 
+  handleChangeGender(value) {
+    console.log(value);
+  }
+
+  handleChangeDay(value) {
+    console.log(value);
+  }
+
+  handleChangeMonth(value) {
+    const year = this.props.form.getFieldValue('year');
+    if (year % 4 === 0 && value === 2) {
+      this.setState(
+        {
+          month_selected: this.state.day_in_february_profit,
+        },
+        () => {
+          const day = this.props.form.getFieldValue('day');
+          if (day > 29) {
+            this.props.form.setFieldsValue({
+              day: 1,
+            });
+          }
+        }
+      );
+      return;
+    }
+    if (value === 2) {
+      this.setState(
+        {
+          month_selected: this.state.day_in_february_normal,
+        },
+        () => {
+          const day = this.props.form.getFieldValue('day');
+          if (day > 28) {
+            this.props.form.setFieldsValue({
+              day: 1,
+            });
+          }
+        }
+      );
+      return;
+    }
+    if (
+      value === 1 ||
+      value === 3 ||
+      value === 5 ||
+      value === 7 ||
+      value === 8 ||
+      value === 10 ||
+      value === 12
+    ) {
+      this.setState({
+        month_selected: this.state.day_in_month_full,
+      });
+      return;
+    }
+    if (value === 4 || value === 6 || value === 9 || value === 11) {
+      this.setState(
+        {
+          month_selected: this.state.day_in_month_short,
+        },
+        () => {
+          const day = this.props.form.getFieldValue('day');
+          if (day > 30) {
+            this.props.form.setFieldsValue({
+              day: 1,
+            });
+          }
+        }
+      );
+    }
+  }
+
+  handleChangeYear(value) {
+    const month = this.props.form.getFieldValue('month');
+    if (value % 4 === 0 && month === 2) {
+      this.setState(
+        {
+          month_selected: this.state.day_in_february_profit,
+        },
+        () => {
+          const day = this.props.form.getFieldValue('day');
+          if (day > 29) {
+            this.props.form.setFieldsValue({
+              day: 1,
+            });
+          }
+        }
+      );
+      return;
+    }
+    if (month === 2) {
+      this.setState(
+        {
+          month_selected: this.state.day_in_february_normal,
+        },
+        () => {
+          const day = this.props.form.getFieldValue('day');
+          if (day > 28) {
+            this.props.form.setFieldsValue({
+              day: 1,
+            });
+          }
+        }
+      );
+    }
+  }
+
+  handleChangeIntent(value) {
+    console.log(value);
+  }
+
   render() {
     const { getFieldDecorator } = this.props.form;
     const { help, visible } = this.state;
     return (
       <Form onSubmit={this.handleSubmit}>
-        <Form.Item>
-          {getFieldDecorator('fullname', {
+        <Form.Item
+          label="Tên"
+          style={{ width: '45%', display: 'inline-block', marginBottom: '0px' }}
+        >
+          {getFieldDecorator('name', {
             rules: [
               {
                 required: true,
-                message: 'Nhập họ tên',
+                message: 'Nhập tên bạn',
               },
             ],
-          })(<Input size="large" placeholder="Họ tên" />)}
+          })(<Input size="large" placeholder="Ví dụ bạn là Trí" />)}
         </Form.Item>
-        <Form.Item>
-          {getFieldDecorator('email', {
-            rules: [
-              {
-                required: true,
-
-                message: 'Sai định dạng email',
-              },
-            ],
+        <Form.Item
+          label="Giới tính"
+          style={{ width: '45%', display: 'inline-block', float: 'right', marginBottom: '0px' }}
+        >
+          {getFieldDecorator('gender', {
+            required: true,
+            message: 'Nhập giới tính',
           })(
-            <Input
-              type="text"
-              size="large"
-              placeholder="Email"
-              onBlur={e => this.validEmailSync(e)}
-            />
+            <Select size="large" placeholder="Bạn là..." onChange={e => this.handleChangeGender(e)}>
+              <Option value="men">Nam</Option>
+              <Option value="woman">Nữ</Option>
+            </Select>
           )}
         </Form.Item>
-        <Form.Item help={help}>
+        <Form.Item label="Ngày sinh" style={{ marginBottom: '0px' }}>
+          {getFieldDecorator('day', {})(
+            <Select
+              style={{ width: '30%' }}
+              size="large"
+              placeholder="Ngày"
+              onChange={e => this.handleChangeDay(e)}
+            >
+              {this.state.month_selected.length > 0
+                ? this.state.month_selected.map((v, i) => {
+                    return (
+                      <Option key={i} value={v}>
+                        {v}
+                      </Option>
+                    );
+                  })
+                : this.state.day_in_month_full.map((v, i) => {
+                    return (
+                      <Option key={i} value={v}>
+                        {v}
+                      </Option>
+                    );
+                  })}
+            </Select>
+          )}
+          {getFieldDecorator('month', {})(
+            <Select
+              style={{ width: '30%', marginLeft: '16px' }}
+              size="large"
+              placeholder="Tháng"
+              onChange={e => this.handleChangeMonth(e)}
+            >
+              {this.state.month.map((v, i) => {
+                return (
+                  <Option key={i} value={v}>
+                    {v}
+                  </Option>
+                );
+              })}
+            </Select>
+          )}
+          {getFieldDecorator('year', {})(
+            <Select
+              style={{ width: '30%', float: 'right' }}
+              size="large"
+              placeholder="Năm"
+              onChange={e => this.handleChangeYear(e)}
+            >
+              {this.state.year.map((v, i) => {
+                return (
+                  <Option key={i} value={v}>
+                    {v}
+                  </Option>
+                );
+              })}
+            </Select>
+          )}
+        </Form.Item>
+        <Form.Item
+          label="Địa chỉ"
+          style={{ width: '45%', display: 'inline-block', marginBottom: '0px' }}
+        >
+          {getFieldDecorator('address', {
+            rules: [
+              {
+                required: true,
+                message: 'Yêu cầu nhập địa chỉ',
+              },
+            ],
+          })(<Input size="large" placeholder="Ví dụ Hà Nội" />)}
+        </Form.Item>
+        <Form.Item
+          label="Bạn ở đây để"
+          style={{ width: '45%', display: 'inline-block', float: 'right', marginBottom: '0px' }}
+        >
+          {getFieldDecorator('intent', {})(
+            <Select size="large" placeholder="Chọn..." onChange={e => this.handleChangeIntent(e)}>
+              <Option value="chat">Chat</Option>
+              <Option value="pal">Kết bạn</Option>
+              <Option value="dating">Hẹn hò</Option>
+            </Select>
+          )}
+        </Form.Item>
+        <Form.Item label="Số điện thoại" style={{ marginBottom: '0px' }}>
+          {getFieldDecorator('phone', {
+            rules: [
+              {
+                required: true,
+                message: 'Yêu cầu nhập số điện thoại',
+              },
+              {
+                pattern: /\d{9}$/,
+                message: 'Nhập sai định dạng hoặc chưa đủ chữ số！',
+              },
+            ],
+          })(<Input size="large" placeholder="Số điện thoại là tài khoản đăng nhập" />)}
+        </Form.Item>
+        <Form.Item help={help} label="Mật khẩu" style={{ marginBottom: '0px' }}>
           <Popover
             content={
               <div style={{ padding: '4px 0' }}>
                 {passwordStatusMap[this.getPasswordStatus()]}
                 {this.renderPasswordProgress()}
-                <div style={{ marginTop: 10 }}>Mât khẩu lớn hơn 6 ký tự</div>
+                <div style={{ marginTop: 10 }}>Mât khẩu ít nhất 6 ký tự</div>
               </div>
             }
             overlayStyle={{ width: 240 }}
@@ -281,10 +745,10 @@ class FormRegister extends PureComponent {
                   validator: this.checkPassword,
                 },
               ],
-            })(<Input size="large" type="password" placeholder="Mật khẩu" />)}
+            })(<Input size="large" type="password" placeholder="Tối thiểu 6 ký tự" />)}
           </Popover>
         </Form.Item>
-        <Form.Item>
+        <Form.Item label="Nhập lại">
           {getFieldDecorator('repassword', {
             rules: [
               {
@@ -301,32 +765,20 @@ class FormRegister extends PureComponent {
             />
           )}
         </Form.Item>
+
         <Form.Item>
-          {getFieldDecorator('phone', {
-            rules: [
-              {
-                required: true,
-                message: 'Yêu cầu nhập số điện thoại',
-              },
-              {
-                pattern: /\d{9}$/,
-                message: 'Nhập sai định dạng hoặc chưa đủ chữ số！',
-              },
-            ],
-          })(<Input size="large" placeholder="Số điện thoại" />)}
-        </Form.Item>
-        <Form.Item>
-          {getFieldDecorator('address', {
-            rules: [
-              {
-                required: true,
-                message: 'Yêu cầu nhập địa chỉ',
-              },
-            ],
-          })(<Input size="large" placeholder="Địa chỉ" />)}
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit" block>
+          <Button
+            size="large"
+            type="primary"
+            style={{
+              background: '#45c000',
+              borderColor: '#45c000',
+              color: '#fff',
+              borderRadius: '30px',
+            }}
+            htmlType="submit"
+            block
+          >
             Đăng ký
           </Button>
         </Form.Item>
@@ -341,7 +793,7 @@ class FormRegister extends PureComponent {
 }))
 class Login extends PureComponent {
   state = {
-    statusPage: false,
+    statusPage: true,
   };
 
   handleClickToggleStatus() {
