f66dd63383d5f06529a7769f8183f3a8edbfeeab
trjvjp97 <trjvjp97@gmail.com>
2019-02-13 12:19:57 +0700

    123

--- models/loginModel.js
+++ models/loginModel.js
@@ -4,7 +4,10 @@ module.exports = {
     password: 'text',
     password_hash_algorithm: 'text',
     password_salt: 'text',
-    user_id: 'uuid',
+    user_id: {
+      type: 'uuid',
+      default: { $db_function: 'uuid()' },
+    },
   },
   key: ['phone', 'user_id'],
 };
