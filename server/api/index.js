76a5c1f4ca684945149c5d61fa98f4cf8ccdd74f
thangph9 <root@localhost.localdomain>
2019-02-14 12:09:26 +0700

    update server

--- server/api/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express'); // eslint-disable-line
-
-const router = express.Router();
-
-router.use('/user', require('./user'));
-router.use('/authentication', require('./authentication'));
-
-module.exports = router;
