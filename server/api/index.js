ef11facbda58f0ef1e71753b34a0fca91eb4dfbc
trjvjp97 <trjvjp97@gmail.com>
2019-02-18 17:17:42 +0700

    up server

--- server/api/index.js
+++ server/api/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 
 router.use('/user', require('./user'));
 router.use('/authentication', require('./authentication'));
+router.use('/profile', require('./profile'));
 
 module.exports = router;
