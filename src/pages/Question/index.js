3989288fc001fd5faa3feae66d6bce69202ac461
trjvjp97 <trjvjp97@gmail.com>
2019-03-03 11:55:01 +0700

    update question

--- src/pages/Question/index.js
+++ src/pages/Question/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-closing-tag-location */
+/* eslint-disable react/jsx-indent */
 /* eslint-disable react/no-access-state-in-setstate */
 /* eslint-disable arrow-body-style */
 /* eslint-disable react/no-array-index-key */
@@ -33,6 +35,7 @@ class Question extends PureComponent {
     effectMain: false,
     statusArrea: true,
     valueInput: '',
+    arrAnswer: [],
   };
 
   componentDidMount() {
@@ -46,23 +49,54 @@ class Question extends PureComponent {
     }, 100);
   }
 
-  onChangeRadio(e) {
+  onChangeRadio(e, q) {
+    let obj = {
+      question: q,
+      answer: e.target.value,
+      type: '2',
+    };
+    let arr = this.state.arrAnswer;
+    arr[arr.length] = obj;
     if (this.state.currentQuestion + 1 < this.state.dataQuestion.length) {
       setTimeout(() => {
         this.setState({
           currentQuestion: this.state.currentQuestion + 1,
+          arrAnswer: arr,
         });
       }, 500);
     }
   }
 
-  onChangeCheckBox(e) {
+  onChangeCheckBox(e, q) {
     this.setState({
       arrCheck: e,
     });
   }
 
-  handleClickNext() {
+  handleClickNext(q, t) {
+    let arr = this.state.arrAnswer;
+
+    if (t === '1') {
+      let obj = {
+        question: q,
+        answer: this.state.valueInput,
+        type: '1',
+      };
+      arr[arr.length] = obj;
+      this.setState({
+        arrAnswer: arr,
+      });
+    } else if (t === '3') {
+      let obj = {
+        question: q,
+        answer: this.state.arrCheck,
+        type: '3',
+      };
+      arr[arr.length] = obj;
+      this.setState({
+        arrAnswer: arr,
+      });
+    }
     if (this.state.currentQuestion + 1 < this.state.dataQuestion.length) {
       this.setState({
         currentQuestion: this.state.currentQuestion + 1,
@@ -72,19 +106,43 @@ class Question extends PureComponent {
   }
 
   handleClickPrev() {
-    this.setState({
-      currentQuestion: this.state.currentQuestion - 1,
-      valueInput: '',
-    });
+    this.setState(
+      {
+        currentQuestion: this.state.currentQuestion - 1,
+        valueInput: '',
+      },
+      () => {
+        let arr = this.state.arrAnswer;
+        arr.splice(arr.length - 1, 1);
+        this.setState({
+          arrAnswer: arr,
+        });
+      }
+    );
   }
 
   handleClickSubmit() {
-    message.info('Phiên bản test chưa thể hoàn tất');
+    message.info('Đã gửi!');
+    /*
+     this.props.dispatch({
+      type:'authentication/sendanswer',
+      payload:{
+        answer:this.state.arrAnswer
+      }
+    })
+   */
   }
 
-  handleChangeInput(e) {
+  handleChangeInput(e, q) {
     this.setState({
       valueInput: e.target.value,
+    });
+  }
+
+  handleClickSkip() {
+    this.setState({
+      currentQuestion: this.state.currentQuestion + 1,
+      valueInput: '',
     });
   }
 
@@ -100,7 +158,6 @@ class Question extends PureComponent {
 
   render() {
     const { currentQuestion, dataQuestion, arrCheck, effectMain, valueInput } = this.state;
-    console.log(dataQuestion);
     return (
       <div style={{ background: '#F3F5F9', marginTop: '-20px', height: '100%' }}>
         <div className={styles['container']}>
@@ -120,7 +177,12 @@ class Question extends PureComponent {
               <div className={styles['current-answer']}>
                 {dataQuestion[currentQuestion].answer &&
                   dataQuestion[currentQuestion].type === '2' && (
-                    <RadioGroup style={{ width: '100%' }} onChange={e => this.onChangeRadio(e)}>
+                    <RadioGroup
+                      style={{ width: '100%' }}
+                      onChange={e =>
+                        this.onChangeRadio(e, dataQuestion[currentQuestion].question_id)
+                      }
+                    >
                       {dataQuestion[currentQuestion].answer.map((v, i) => {
                         return (
                           <Radio
@@ -140,7 +202,9 @@ class Question extends PureComponent {
                   dataQuestion[currentQuestion].type === '3' && (
                     <Checkbox.Group
                       style={{ width: '100%' }}
-                      onChange={e => this.onChangeCheckBox(e)}
+                      onChange={e =>
+                        this.onChangeCheckBox(e, dataQuestion[currentQuestion].question_id)
+                      }
                     >
                       {dataQuestion[currentQuestion].answer.map((v, i) => {
                         return (
@@ -161,33 +225,52 @@ class Question extends PureComponent {
                   <div style={{ padding: '30px' }} className={styles['list-answer']}>
                     <TextArea
                       value={valueInput}
-                      onChange={e => this.handleChangeInput(e)}
+                      onChange={e =>
+                        this.handleChangeInput(e, dataQuestion[currentQuestion].question_id)
+                      }
                       placeholder="Do something..."
                       rows={3}
                     />
                   </div>
                 )}
               </div>
-              {currentQuestion > 0 && (
+              {currentQuestion > 0 ? (
                 <div onClick={() => this.handleClickPrev()} className={styles['prev-check']}>
                   <Icon style={{ fontSize: '25px', marginRight: '10px' }} type="arrow-left" />
                   Quay lại
                 </div>
+              ) : (
+                <div className={styles['prev-check']} />
               )}
-              {currentQuestion === dataQuestion.length - 1 && (
+              {currentQuestion === dataQuestion.length - 1 ? (
                 <div onClick={() => this.handleClickSubmit()} className={styles['button-submit']}>
                   <Icon style={{ fontSize: '25px', marginRight: '10px' }} type="check-circle" />
                   Hoàn tất
                 </div>
+              ) : (
+                <div onClick={() => this.handleClickSkip()} className={styles['button-submit']}>
+                  <Icon style={{ fontSize: '25px', marginRight: '10px' }} type="check" />
+                  Bỏ qua
+                </div>
               )}
               {((dataQuestion[currentQuestion].type === '3' && arrCheck.length > 0) ||
                 (dataQuestion[currentQuestion].type === '1' && valueInput.length > 0)) &&
-                currentQuestion + 1 < dataQuestion.length && (
-                  <div onClick={() => this.handleClickNext()} className={styles['next-check']}>
-                    Tiếp tục{' '}
-                    <Icon style={{ fontSize: '25px', marginLeft: '10px' }} type="arrow-right" />
-                  </div>
-                )}
+              currentQuestion + 1 < dataQuestion.length ? (
+                <div
+                  onClick={() =>
+                    this.handleClickNext(
+                      dataQuestion[currentQuestion].question_id,
+                      dataQuestion[currentQuestion].type
+                    )
+                  }
+                  className={styles['next-check']}
+                >
+                  Tiếp tục{' '}
+                  <Icon style={{ fontSize: '25px', marginLeft: '10px' }} type="arrow-right" />
+                </div>
+              ) : (
+                <div className={styles['next-check']} />
+              )}
             </div>
           )}
         </div>
