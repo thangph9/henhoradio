c7f1c2a3600e58ae68bff1ea6aeae1e3b17e9ff7
trjvjp97 <trjvjp97@gmail.com>
2019-03-13 02:18:40 +0700

    fix bug

--- server/api/images.js
+++ server/api/images.js
@@ -19,43 +19,54 @@ const sizeOf = require('image-size');
 const models = require('../settings');
 
 const Uuid = models.datatypes.Uuid;
-
-function uploadFileAvatar(req, res) {
-  let dimensions = '';
-  const isValid = true;
-  upload.single('file')(req, res, function(err) {
-    if (err) {
-      console.log(err);
-      return res.send({ status: 'error' });
+function imageByTri(req, res) {
+  let image = null;
+  let imageid = '';
+  async.series(
+    [
+      function(callback) {
+        try {
+          let rawImage = req.params.imageid;
+          imageid = models.uuidFromString(rawImage);
+        } catch (e) {
+          res.contentType('image/jpeg');
+          return res.end('', 'binary');
+        }
+        callback(null, null);
+      },
+      function(callback) {
+        try {
+          models.instance.images.find({ image_id: imageid }, function(err, img) {
+            if (img && img.length > 0) {
+              image = img[0].image;
+            }
+            callback(err, null);
+          });
+        } catch (error) {
+          console.log(error);
+          callback(error, null);
+        }
+      },
+    ],
+    function(err, result) {
+      try {
+        if (err) {
+          res.contentType('image/jpeg');
+          res.end('', 'binary');
+        } else if (image != null) {
+          res.writeHead(200, { 'Content-Type': 'image/jpg' });
+          res.end(image, 'binary');
+        } else {
+          res.contentType('image/jpeg');
+          res.end('', 'binary');
+        }
+      } catch (error) {
+        console.log(error);
+      }
     }
-    let imageid = Uuid.random();
-    try {
-      let file = req.files[0];
-      let image = file.buffer;
-      let options = {
-        filename: file.originalname,
-        size: `${file.size}`,
-        encoding: file.encoding,
-        mimetype: file.mimetype,
-      };
-      let image_object = {
-        image_id: imageid,
-        image,
-        options,
-        createat: new Date(),
-        face_active: false,
-      };
-      dimensions = sizeOf(image);
-      let object = image_object;
-      let instance = new models.instance.images(object);
-      let save = instance.save(function(err) {});
-    } catch (e) {
-      console.log(e);
-      return res.send({ status: 'error' });
-    }
-    res.send({ status: 'ok', file: { imageid, isValid } });
-  });
+  );
 }
+
 const router = express.Router();
-router.get('/images/ft/:imageid', uploadFileAvatar);
+router.get('/images/ft/:imageid', imageByTri);
 module.exports = router;
