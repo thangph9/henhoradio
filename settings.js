33b9793c73effa34d50330d64e3d80d2ecc6b566
trjvjp97 <trjvjp97@gmail.com>
2019-06-03 16:48:57 +0700

    update

--- settings.js
+++ settings.js
@@ -2,25 +2,4 @@
 /* eslint-disable prefer-template */
 const models = require('express-cassandra');
 
-models.setDirectory(__dirname + '/models').bind(
-  {
-    clientOptions: {
-      contactPoints: ['127.0.0.1'],
-      protocalOptions: { port: 9042 },
-      keyspace: 'hhr',
-      queryOptions: { consistency: models.consistencies.one },
-      // authProvider: new models.driver.auth.PlainTextAuthProvider('my_user', 'my_password')
-    },
-    ormOptions: {
-      defaultReplicationStrategy: {
-        class: 'SimpleStrategy',
-        replication_factor: 1,
-      },
-      migration: 'safe',
-    },
-  },
-  err => {
-    if (err) throw err;
-  }
-);
 module.exports = models;
