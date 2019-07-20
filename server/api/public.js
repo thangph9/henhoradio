ee054561a458762e94b44a5aefeee671c56d6684
root <root@localhost.localdomain>
2019-07-20 00:35:50 -0400

    a

--- server/api/public.js
+++ server/api/public.js
@@ -5,8 +5,8 @@ const fs = require('fs');
 const jwt = require('jsonwebtoken');
 const request = require('request'); // eslint-disable-line
 const express = require('express');
+const moment = require('moment');
 const models = require('../settings');
-
 // const Uuid = models.datatypes.Uuid;
 
 const jwtpublic = fs.readFileSync('./ssl/jwtpublic.pem', 'utf8');
@@ -14,9 +14,15 @@ const verifyOptions = {
   expiresIn: '30d',
   algorithm: ['RS256'],
 };
+/**
+ * Search Member, Users active api/DATA/DT?{query}
+ * query page,date,radio,gender,sort
+ * return list of member and users filter sorted
+ */
 
 function getMembers(req, res) {
   const token = req.headers['x-access-token'];
+  const { query } = req;
   let legit = {};
   const tasks = [
     function checkAuth(callback) {
@@ -56,7 +62,9 @@ function getMembers(req, res) {
       e.name = e.fullname;
       e.ucode = e.uniqueid;
       e.membersid = e.id;
+      e.timeup = e.created;
     });
+
     let rs = [];
     if (members && users) {
       rs = jsonMembers.concat(jsonUser);
@@ -65,10 +73,48 @@ function getMembers(req, res) {
     } else {
       rs = jsonMembers;
     }
+    // Filter and sort result
+
+    const { radio, gender, sort, date } = query;
+
+    let listMember = rs.filter(e => {
+      if (radio === 'ALL') return e;
+      if (radio === 'VALID') return e.id !== undefined;
+      return e.location === radio;
+    });
+    listMember = listMember.filter(e => {
+      if (gender === 'ALL') return e;
+      return e.gender.toUpperCase() === gender.toUpperCase();
+    });
+    // const defaultSort=listMember;
+    if (sort === 'newest') {
+      listMember.sort((e, f) => {
+        const eTime = new Date(e.timeup).getTime();
+        const jTime = new Date(f.timeup).getTime();
+        return jTime - eTime;
+      });
+    }
+    if (sort === 'special') {
+      listMember = listMember.filter(e => e.species && e.species === 1);
+    }
+    if (sort === 'default') {
+      listMember.sort((e, f) => {
+        console.log(e.timeup, f.timeup);
+        const eTime = new Date(e.timeup).getTime();
+        const jTime = new Date(f.timeup).getTime();
+        return jTime - eTime;
+      });
+    }
+    if (date) {
+      listMember = listMember.filter(e => {
+        const eDate = moment(e.timeup).format('DD_MM_YYYY');
+        return eDate === date;
+      });
+    }
 
     return res.json({
       status: 'ok',
-      data: rs,
+      data: listMember,
     });
   });
 }
--- server/models/membersModel.js
+++ server/models/membersModel.js
@@ -20,8 +20,44 @@ module.exports = {
     mobile: 'text',
     gender: 'text',
     timeup: 'timestamp',
-    createat: 'timestamp',
     createby: 'text',
+    created: {
+      type: 'timestamp',
+      default: { $db_function: 'toTimestamp(now())' },
+    },
   },
-  key: ['membersid'],
+  key: [['membersid'], 'created'],
+  clustering_order: { created: 'desc' },
+  materialized_views: {
+    view_member: {
+      select: [
+        'membersid',
+        'ucode',
+        'gcode',
+        'name',
+        'day',
+        'month',
+        'year',
+        'audio',
+        'location',
+        'description',
+        'job',
+        'relationship',
+        'address',
+        'mobile',
+        'gender',
+        'timeup',
+      ],
+      key: ['membersid', 'created'],
+    },
+  },
+  options: {
+    timestamps: {
+      createdAt: 'created_at', // defaults to createdAt
+      updatedAt: 'updated_at', // defaults to updatedAt
+    },
+    versions: {
+      key: '__v', // defaults to __v
+    },
+  },
 };
