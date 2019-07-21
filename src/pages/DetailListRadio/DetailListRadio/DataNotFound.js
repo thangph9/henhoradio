b703f1f2fe4fc5d54ab85b792d78b19e961c2538
root <root@localhost.localdomain>
2019-07-21 06:12:18 -0400

    a

--- src/pages/DetailListRadio/DetailListRadio/DataNotFound.js
+++
@@ -1,8 +0,0 @@
-import React, { PureComponent } from 'react';
-
-class DataNotFound extends PureComponent {
-  render() {
-    return <i>Không tìm thấy dữ liệu</i>;
-  }
-}
-export default DataNotFound;
